b7cf9bfbd854a3a11100b5b8c973b469cf8174e8
MarGama <94371820+MarGamaDev@users.noreply.github.com>
2026-02-01 20:00:05 +0100

    I AM THE MASTER OF SOUP

--- flow/Scenes/GameManager.gd
+++ flow/Scenes/GameManager.gd
@@ -6,12 +6,27 @@ const TIME_SHADER_MATERIAL = preload("uid://yjrxvhfw04t4")
 @onready var time_orb: TimeOrb = $"Time Orb"
 @onready var player: Player = $Player
 
+@onready var soup: TileMapLayer = $Soup
+@onready var soup_bowl: Node2D = $"Soup Bowl"
+const SOUP_TILE = preload("uid://b72oeph4tcfxj")
+
 @export var time_area_radius: float = 50
 
 func _ready() -> void:
 	TIME_SHADER_MATERIAL.set_shader_parameter("time_area_radius", time_area_radius)
 	time_orb.set_radius(time_area_radius)
 	player.initialize(time_orb)
+	
+	# configure soup
+	for position in soup.get_used_cells():
+		var data = soup.get_cell_tile_data(position)
+		var index: int = data.get_custom_data("soup_index")
+		if index != 5:
+			var tile: SoupTile = SOUP_TILE.instantiate();
+			soup_bowl.add_child(tile)
+			tile.global_position = Vector2(position.x * 16 + 8, position.y * 16 + 8)
+			tile.Initialize(index)
+			soup.erase_cell(position)
 
 func _process(_delta: float) -> void:
 	TIME_SHADER_MATERIAL.set_shader_parameter("time_area_coords", time_orb.global_position)
---
+++ flow/Terrain/Soup.gd
@@ -0,0 +1,17 @@
+class_name SoupTile
+extends AnimatedSprite2D
+
+@onready var rand: RandomNumberGenerator = RandomNumberGenerator.new()
+
+var animation_index: String
+
+func Initialize(index: int) -> void:
+	animation_index = str(index)
+	speed_scale = rand.randf_range(0.8, 1.2);
+
+func _on_entity_center_on_update_timezone(enabled: bool) -> void:
+	print("me")
+	if enabled:
+		play(animation_index)
+	else:
+		pause()
